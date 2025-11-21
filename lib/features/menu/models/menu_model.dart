1472d17c7b290f30498ca766e378cadc87f90aff
SychevOS <ysgsks.5@gmail.com>
2025-11-22 00:50:55 +0300

    Изменения в menu_model.dart

--- lib/features/menu/models/menu_model.dart
+++ lib/features/menu/models/menu_model.dart
@@ -3,36 +3,33 @@ class MenuItem {
   final String name;
   final String category;
   final double price;
-  final String? description;
   final bool isAvailable;
-
+  final String imageUrl;
 
   MenuItem({
     required this.id,
     required this.name,
     required this.category,
     required this.price,
-    this.description,
     this.isAvailable = true,
-
+    required this.imageUrl,
   });
 
   MenuItem copyWith({
+    String? id,
     String? name,
     String? category,
     double? price,
-    String? description,
     bool? isAvailable,
     String? imageUrl,
   }) {
     return MenuItem(
-      id: id,
+      id: id ?? this.id,
       name: name ?? this.name,
       category: category ?? this.category,
       price: price ?? this.price,
-      description: description ?? this.description,
       isAvailable: isAvailable ?? this.isAvailable,
-
+      imageUrl: imageUrl ?? this.imageUrl,
     );
   }
 }
